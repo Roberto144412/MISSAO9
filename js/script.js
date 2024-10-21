476cf44a44bbe0e0cc4709f804d90155dad2edec
Roberto144412 <161595657+Roberto144412@users.noreply.github.com>
2024-10-21 20:15:26 -0300

    ss

--- js/script.js
+++ js/script.js
@@ -35,7 +35,13 @@ function mostraAlternativas() {
 function respostaSelecionada(opcaoSelecionada) {
     const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + " ";
-    atual++;
+    if(opcaoSelecionada.proxima !== undefined){
+        atual = opcaoSelecionada.proxima;
+    }else{
+        mostraResultado();
+        return;
+    }
+
     mostraPergunta();
 }
 
